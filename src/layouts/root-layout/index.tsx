dfaba568bd665c85ad766af24e409222bc0e18b6
AB <abaayous.oussama@gmail.com>
2023-09-20 15:35:17 +0100

    started layouts

---
+++ src/layouts/root-layout/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import TeamSwitcher from "./components/team-switcher";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <div>
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <TeamSwitcher />
+          {/* <MainNav className="mx-6" /> */}
+          <div className="ml-auto flex items-center space-x-4">
+            {/* <Search /> */}
+            {/* <UserNav /> */}
+          </div>
+        </div>
+      </div>
+      <div className="">{children}</div>
+    </div>
+  );
+}
